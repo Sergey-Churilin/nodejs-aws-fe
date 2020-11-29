fa68c34954763c1af112cc3da207e68bbfd89c66
Siarhei.Churylin <Siarhei.Churylin@thomsonreuters.com>
2020-11-29 12:44:48 +0300

    Added auth headers

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,10 +1,10 @@
 
 const API_PATHS = {
-  product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  product: 'https://ls2z4pea12.execute-api.eu-west-1.amazonaws.com/dev',
+  order: 'https://ls2z4pea12.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://g61r9jcbo2.execute-api.eu-west-1.amazonaws.com/dev',
+  bff: 'https://ls2z4pea12.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'http://sergey-churilin-cart-api-dev.eu-west-1.elasticbeanstalk.com/api'
 };
 
 export default API_PATHS;
