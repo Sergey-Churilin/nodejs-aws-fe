515650cebaee6d9fb1cbe7b5e64ab6d2c65e982d
Siarhei.Churylin <Siarhei.Churylin@thomsonreuters.com>
2020-11-29 12:44:48 +0300

    Added auth headers

--- src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
+++ src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
@@ -32,8 +32,12 @@ export default function CSVFileImport({url, title}: CSVFileImportProps) {
 
   const uploadFile = async (e: any) => {
       // Get the presigned URL
+      const authorization_token = localStorage.getItem('authorization_token')
       const response = await axios({
         method: 'GET',
+        headers: {
+            Authorization: authorization_token
+        },
         url,
         params: {
           name: encodeURIComponent(file.name)
