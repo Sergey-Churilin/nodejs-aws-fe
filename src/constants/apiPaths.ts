ee41eb77ae20d2dbdd3f73c74598f52a919207cd
Siarhei.Churylin <Siarhei.Churylin@thomsonreuters.com>
2020-12-08 20:06:35 +0300

    Updated url to cart api

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,7 @@ const API_PATHS = {
   order: 'https://ls2z4pea12.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://g61r9jcbo2.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://ls2z4pea12.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'http://sergey-churilin-cart-api-dev.eu-west-1.elasticbeanstalk.com/api'
+  cart: 'https://nucp1a5r58.execute-api.eu-west-1.amazonaws.com/dev/api'
 };
 
 export default API_PATHS;
